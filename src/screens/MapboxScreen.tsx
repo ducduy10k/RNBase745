588c25098c2a00b98f3c92c7c04629333da6044b
Nguyễn Đức Duy <59301500+ducduy10k@users.noreply.github.com>
2024-08-20 23:09:53 +0700

    react asset

--- src/screens/MapboxScreen.tsx
+++ src/screens/MapboxScreen.tsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Dimensions, StyleSheet, TouchableOpacity, View} from 'react-native';
-// import Mapbox from '@rnmapbox/maps';
+import Mapbox from '@rnmapbox/maps';
 import CustomIcon from '../components/CustomIcon';
-// Mapbox.setAccessToken(
-//   'sk.eyJ1IjoiYmV0YXBjaG9pMTBrIiwiYSI6ImNsd2o1cGRmcTBxZGsyaXBmd2J2emRwc28ifQ.sg6Y6R2AWkqU5v0HwXHCyQ',
-// );
+import {
+  CameraProps,
+  CameraRef,
+} from '@rnmapbox/maps/lib/typescript/src/components/Camera';
+Mapbox.setAccessToken(
+  'sk.eyJ1IjoiYmV0YXBjaG9pMTBrIiwiYSI6ImNsd2o1cGRmcTBxZGsyaXBmd2J2emRwc28ifQ.sg6Y6R2AWkqU5v0HwXHCyQ',
+);
 
 interface MapboxScreenProps {
   navigation: any;
 }
 
 const MapboxScreen: React.FC<MapboxScreenProps> = ({navigation}) => {
+  let cameraRef = useRef<CameraRef | null>(null);
+  const [mapCameraState, setMapCameraState] = useState<CameraProps>({
+    centerCoordinate: [105.80946796710893, 21.02346518415829],
+    zoomLevel: 16,
+    followUserLocation: true,
+    padding: {
+      paddingBottom: 10,
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingTop: 10,
+    },
+    animationDuration: 1000,
+  });
   const backHandler = () => {
     navigation.pop();
   };
+
   return (
     <View style={styles.page}>
-      {/* <Mapbox.MapView
+      <Mapbox.MapView
         style={styles.map}
         styleURL={'mapbox://styles/mapbox/streets-v12'}
         testID={'mapbox'}>
-      </Mapbox.MapView> */}
+        <Mapbox.Camera
+          ref={ref => (cameraRef.current = ref)}
+          defaultSettings={mapCameraState}
+          ></Mapbox.Camera>
+      </Mapbox.MapView>
       <View style={styles.backBtn}>
-          <TouchableOpacity onPress={() => backHandler()}>
-            <CustomIcon name="left" lib="Ant" size={16} color={'white'} />
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity onPress={() => backHandler()}>
+          <CustomIcon name="left" lib="Ant" size={16} color={'white'} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
