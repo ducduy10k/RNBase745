c1038879c40aa847097454c3c936fe8de46002fa
Nguyễn Đức Duy <59301500+ducduy10k@users.noreply.github.com>
2024-08-24 14:02:32 +0700

    init database

---
+++ src/db/tracker.db.ts
@@ -0,0 +1,38 @@
+import * as SQLite from 'react-native-quick-sqlite';
+
+const database = SQLite.open({
+  name: 'shopping',
+});
+
+export function init() {
+  return new Promise((resolve, reject) => {
+    database.transaction(tx => {
+      tx.executeAsync(
+        `CREATE TABLE IF NOT EXISTS tracker (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                description TEXT,
+                start_date INTEGER,
+                end_date INTEGER,
+                lat_start REAL,
+                lng_start REAL,
+                lat_end REAL,
+                lng_end REAL,
+                geojson TEXT,
+                created_at INTEGER NOT NULL,
+                updated_at INTEGER,
+                created_by TEXT NOT NULL,
+                updated_by TEXT
+            )`,
+        [],
+      )
+        .then(result => {
+          console.log(result);
+          resolve(result);
+        })
+        .catch(error => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  });
+}
